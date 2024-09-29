d7452d9f6fd0d477b9df52e83b5b7d93f9a70b71
nguyenducmy <86357942+nguyenducmy@users.noreply.github.com>
2024-09-29 13:52:21 +0700

    Update DemoApplication.java

--- spring-demo/src/main/java/com/example/demo/DemoApplication.java
+++ spring-demo/src/main/java/com/example/demo/DemoApplication.java
@@ -9,11 +9,57 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(DemoApplication.class, args);
-//        Object bean1 = context.getBean("checkCIC");
-//        Object bean2 = context.getBean("sendApprove");
-//        Object bean3 = context.getBean("disbursement");
-//        System.out.println(bean1.getClass().getName());
 
+     // Run once tine only to input Group , User   
+     @Bean
+    InitializingBean usersAndGroupsInitializer(final IdentityService identityService) {
+
+        return () -> {
+	    Group validators = identityService.newGroup("validators");
+	    validators.setName("validators");
+	    validators.setType("security-role");
+	    identityService.saveGroup(validators);
+
+	    Group approvers = identityService.newGroup("approvers");
+	    approvers.setName("approvers");
+	    approvers.setType("security-role");
+	    identityService.saveGroup(approvers);
+
+	    Group payers = identityService.newGroup("payers");
+	    payers.setName("payers");
+	    payers.setType("security-role");
+	    identityService.saveGroup(payers);
+
+	    Group vendors = identityService.newGroup("vendors");
+	    vendors.setName("vendors");
+	    vendors.setType("security-role");
+	    identityService.saveGroup(vendors);
+
+	    User a = identityService.newUser("validator");
+	    a.setPassword("pw");
+	    identityService.saveUser(a);
+	    identityService.createMembership(a.getId(), validators.getId());
+
+	    User b = identityService.newUser("approver");
+	    b.setPassword("pw");
+	    identityService.saveUser(b);
+	    identityService.createMembership(b.getId(), approvers.getId());
+
+	    User c = identityService.newUser("payer");
+	    c.setPassword("pw");
+	    identityService.saveUser(c);
+	    identityService.createMembership(c.getId(), payers.getId());
+
+	    User v = identityService.newUser("vendor");
+	    v.setPassword("pw");
+	    identityService.saveUser(v);
+	    identityService.createMembership(v.getId(), vendors.getId());
+
+	    User admin = identityService.newUser("admin");
+	    admin.setPassword("admin");
+	    identityService.saveUser(admin);
+	};
+    }
 
     }
 
